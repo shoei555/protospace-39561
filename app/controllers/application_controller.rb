6a616d59e823c7e784a4d3a4dbc7e2672cbffd8e
shoei555 <136680372+shoei555@users.noreply.github.com>
2023-07-03 17:43:44 +0900

    ProtoSpaceのユーザー管理機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :move_to_index, except: [:index, :new ]
+
+  private
+  def configure_permitted_parameters  # メソッド名は慣習
+    # deviseのUserモデルにパラメーターを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+  end 
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
 end
