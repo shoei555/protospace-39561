29e18358ea194e67be010d73d0ffe516b3edc0d9
shoei555 <136680372+shoei555@users.noreply.github.com>
2023-07-04 17:42:43 +0900

    ProtoSpaceのその他の機能を実装しよう

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :move_to_index, except: [:index, :new ,:create]
 
   private
   def configure_permitted_parameters  # メソッド名は慣習
     # deviseのUserモデルにパラメーターを許可
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
   end 
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to root_path
-    end
-  end
 
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,14 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @prototypes = Prototype.all 
   end
 
   def show
     @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
     
   def new
@@ -21,7 +25,12 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    def move_to_index
+      @prototype = Prototype.find(params[:id])
+      unless user_signed_in? && current_user.id == @prototype.user_id
+        redirect_to root_path
+      end
+    end
   end
   
   def update
@@ -44,4 +53,11 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
 end
